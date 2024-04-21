1ca0e977e68e285039d1404870d95c8eb876fe60
theperfectpr <kbarua.dev@gmail.com>
2024-04-21 18:34:53 +0530

    changes image processing logic and auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,38 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image) => (
+        <div key={image.id} className="flex w-48 flex-col">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="flex w-48 flex-col">
-            <img src={image.url} />
-            <div>{image.name}</div>
+        <SignedOut>
+          <div className="h-full w-full text-center text-2xl">
+            Please sign in to view the images !
           </div>
-        ))}
+        </SignedOut>
+
+        <SignedIn>
+          <Images />
+        </SignedIn>
       </div>
     </main>
   );
