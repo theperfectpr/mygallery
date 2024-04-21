753005aee3c905c725966c5a861303401748a257
theperfectpr <kbarua.dev@gmail.com>
2024-04-21 18:01:09 +0530

    remove mockData and use actual data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,18 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://kbreactappbuck.s3.ap-south-1.amazonaws.com/pic1.png",
-  "https://kbreactappbuck.s3.ap-south-1.amazonaws.com/IMG_20190531_163347_176_Original.jpg",
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {posts.map((post) => (
-          <div key={post.id}>{post.name}</div>
-        ))}
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} alt="image" />
+        {images.map((image) => (
+          <div key={image.id} className="flex w-48 flex-col">
+            <img src={image.url} />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>
