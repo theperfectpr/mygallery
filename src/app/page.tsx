2936cd420af3031b8eedbd57af65f7eeb21fb913
theperfectpr <kbarua.dev@gmail.com>
2024-04-21 17:35:03 +0530

    DB-drizzle setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://kbreactappbuck.s3.ap-south-1.amazonaws.com/pic1.png",
   "https://kbreactappbuck.s3.ap-south-1.amazonaws.com/IMG_20190531_163347_176_Original.jpg",
@@ -8,12 +10,19 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48">
+        {posts.map((post) => (
+          <div key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={image.id + "-" + index} className="w-48">
             <img src={image.url} alt="image" />
           </div>
         ))}
